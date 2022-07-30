51139cf1b84cee9a7eaf8819dc1de6fdd5c3ac6a
Jon Christie <jonpchristie@gmail.com>
2022-07-30 03:10:56 -0400

    portfolio link

--- src/data/socialsData.js
+++ src/data/socialsData.js
@@ -1,11 +1,11 @@
 export const socialsData = {
 	github: 'https://github.com/mathcodes/',
-	facebook: 'https://www.facebook.com/jonpchristie',
+	facebook: 'https://www.facebook.com/fullstack11235',
 	linkedIn: 'https://www.linkedin.com/in/jonpchristie/',
 	youtube: 'https://www.youtube.com/channel/UC5GFnN-lv8Yuqc9O3b79k6g',
 	instagram: 'https://www.instagram.com/fullstack11235/',
 	codepen: 'https://codepen.io/mathcodes',
-	twitter: 'https://https://twitter.com/jonpchristie/',
+	twitter: 'https://twitter.com/fullstack112358/',
 	reddit: 'https://www.reddit.com/user/fullstack11235813',
 	stackOverflow: 'https://stackoverflow.com/users/11303840/jon-christie',
 	replit: 'https://replit.com/@fullstack11235',
