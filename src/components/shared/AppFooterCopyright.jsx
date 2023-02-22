2281fef52700b97000f27469629880167a98c32a
mathcodes <jonpchristie@gmail.com>
2023-02-22 17:14:21 -0500

    editing color theme

--- src/components/shared/AppFooterCopyright.jsx
+++ src/components/shared/AppFooterCopyright.jsx
@@ -1,23 +1,25 @@
 function AppFooterCopyright() {
 	return (
 		<div className="font-general-regular flex justify-center items-center text-center">
-			<div className="text-lg text-ternary-dark dark:text-ternary-light">
-				&copy; {new Date().getFullYear()}
+			<div className="flex flex-row text-lg text-ternary-dark dark:text-ternary-light">
 				<a
-					href="https://github.com/mathcodes/react-tailwindcss-portfolio"
-					target="__blank"
-					className="hover:underline hover:text-indigo-600 dark:hover:text-indigo-300 ml-1 duration-500"
-				>
-					React & Tailwind CSS Portfolio
-				</a>
-				.
-				<a
+ style={{height: '3rem', paddingBottom: '1em'}}
 					href="https://www.jonchristie.net"
 					target="__blank"
-					className="text-secondary-dark dark:text-secondary-light font-medium uppercase hover:underline hover:text-indigo-600 dark:hover:text-indigo-300 ml-1 duration-500"
+					className=" flex text-secondary-dark dark:text-secondary-light font-medium uppercase hover:underline hover:text-indigo-600 dark:hover:text-indigo-300 ml-1 duration-500"
 				>
-					Jon
+					<img src="https://www.jonchristie.net/favicon.png" alt="Jon Christie Logo" className="p-40" />
 				</a>
+				&nbsp;
+				&copy; {new Date().getFullYear()}
+				<a
+					href="https://github.com/mathcodes/mathcodes.github.io"
+					target="__blank"
+					className="flex hover:underline hover:text-indigo-600 dark:hover:text-indigo-300 ml-1 duration-500"
+				>
+					Jon Christie
+				</a>
+
 			</div>
 		</div>
 	);
