63c44997a23f4188ac210d0fac59e7a11b098683
mathcodes <jonpchristie@gmail.com>
2023-02-22 18:10:57 -0500

    links updated

--- src/components/projects/ProjectSingle.jsx
+++ src/components/projects/ProjectSingle.jsx
@@ -12,8 +12,7 @@ const ProjectSingle = ({ title, category, image }) => {
 				delay: 0.15,
 			}}
 		>
-			<Link to="/projects/single-project" aria-label="Single Project">
-				<div className="rounded-xl shadow-lg hover:shadow-xl cursor-pointer mb-10 sm:mb-0 bg-secondary-light dark:bg-ternary-dark">
+ 				<div className="rounded-xl shadow-lg hover:shadow-xl cursor-pointer mb-10 sm:mb-0 bg-secondary-light dark:bg-ternary-dark">
 					<div>
 						<img
 							src={image}
@@ -30,7 +29,7 @@ const ProjectSingle = ({ title, category, image }) => {
 						</span>
 					</div>
 				</div>
-			</Link>
+
 		</motion.div>
 	);
 };
